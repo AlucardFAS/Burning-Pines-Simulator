4021eeb19584cb9d2165bb2bd01003490dcaa210
LeoGajardo <leonardo@gajardo.com.br>
2018-12-08 18:40:26 -0200

    memory leak collision

--- Assets/Scripts/ControlFire.cs
+++ Assets/Scripts/ControlFire.cs
@@ -1,16 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//[RequireComponent(typeOf(ParticleSystem))]
 
 public class ControlFire : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public ParticleSystem part;
+    public Vector3 positionColl;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (rb)
+            {
+                Vector3 pos = collisionEvents[i].intersection;
+                Burn(pos);
+            }
+            i++;
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Burn(Vector3 positionColl)
+	{
+		GameObject NewFireParticle = Instantiate(part.gameObject, positionColl, Quaternion.identity);
+        var newFire = NewFireParticle.GetComponent<ParticleSystem>();
+        var a = newFire.main;
+        a.maxParticles = 1;
+        NewFireParticle.GetComponent<ParticleSystem>().Play();
+
 	}
 }
