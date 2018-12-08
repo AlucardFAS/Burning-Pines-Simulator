89321fcdcbbf23e28fb80f70881a0b16ff3f97ce
LeoGajardo <leonardo@gajardo.com.br>
2018-12-08 19:28:50 -0200

    fixing script

--- Assets/Scripts/ControlFire.cs
+++ Assets/Scripts/ControlFire.cs
@@ -2,40 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-<<<<<<< HEAD
-    public ParticleSystem part;
-    public Vector3 positionColl;
-    public List<ParticleCollisionEvent> collisionEvents;
-
-    void Start()
-    {
-        part = GetComponent<ParticleSystem>();
-        collisionEvents = new List<ParticleCollisionEvent>();
-    }
-
-    void OnParticleCollision(GameObject other)
-    {
-        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
-
-        Rigidbody rb = other.GetComponent<Rigidbody>();
-        int i = 0;
-
-        while (i < numCollisionEvents)
-        {
-            if (rb)
-            {
-                Vector3 pos = collisionEvents[i].intersection;
-                Burn(pos);
-            }
-            i++;
-        }
-    }
-	
-	void Burn(Vector3 positionColl)
-	{
-		GameObject NewFireParticle = Instantiate(part.gameObject, positionColl, Quaternion.identity);
-=======
 public class ControlFire : MonoBehaviour
 {
 
@@ -51,6 +17,9 @@ public class ControlFire : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+
+        //HEAD
+
         int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
 
         Rigidbody rb = other.GetComponent<Rigidbody>();
@@ -70,15 +39,9 @@ public class ControlFire : MonoBehaviour
     void Burn(Vector3 positionColl)
     {
         GameObject NewFireParticle = Instantiate(part.gameObject, positionColl, Quaternion.identity);
->>>>>>> Desfazendo_Merda
         var newFire = NewFireParticle.GetComponent<ParticleSystem>();
         var a = newFire.main;
         a.maxParticles = 1;
         NewFireParticle.GetComponent<ParticleSystem>().Play();
-<<<<<<< HEAD
-	}
-=======
-
     }
->>>>>>> Desfazendo_Merda
 }
