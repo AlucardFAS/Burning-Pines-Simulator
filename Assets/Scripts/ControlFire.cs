be33ac0af9750f982bcdad2732cd5e17014d092c
LeoGajardo <leonardo@gajardo.com.br>
2018-12-08 19:21:27 -0200

    voltando pasta library

--- Assets/Scripts/ControlFire.cs
+++ Assets/Scripts/ControlFire.cs
@@ -1,26 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //[RequireComponent(typeOf(ParticleSystem))]
 
-public class ControlFire : MonoBehaviour {
+public class ControlFire : MonoBehaviour
+{
 
-	public GameObject FireParticle;
-	public Vector3 positionColl;
-    
-    void OnCollisionEnter(Collision coll) 
+    public ParticleSystem part;
+    public Vector3 positionColl;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    void Start()
     {
-		/*if (coll.collider.CompareTag("Plane")) 
-		{
-			
-		}*/
-		positionColl = coll.transform.position;
-		Burn(positionColl);
-	}
-	
-	void Burn(Vector3 positionColl)
-	{
-		GameObject NewFireParticle = Instantiate(FireParticle, positionColl, Quaternion.identity);
-		NewFireParticle.GetComponent<ParticleSystem>().Play();
-	}
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        int i = 0;
+
+        while (i < numCollisionEvents)
+        {
+            if (rb)
+            {
+                Vector3 pos = collisionEvents[i].intersection;
+                Burn(pos);
+            }
+            i++;
+        }
+    }
+
+    void Burn(Vector3 positionColl)
+    {
+        GameObject NewFireParticle = Instantiate(part.gameObject, positionColl, Quaternion.identity);
+        var newFire = NewFireParticle.GetComponent<ParticleSystem>();
+        var a = newFire.main;
+        a.maxParticles = 1;
+        NewFireParticle.GetComponent<ParticleSystem>().Play();
+
+    }
 }
