3337cb499fe6f414e590940a23563e08afe4d097
AlucardFAS <fernandoimp@outlook.com.br>
2018-12-09 16:51:48 -0200

    terrain adjusts; init mouse controller

---
+++ Assets/Scripts/InitFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitFire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+            {
+				Debug.Log("Pressed button.");
+            }
+
+	}
+}
