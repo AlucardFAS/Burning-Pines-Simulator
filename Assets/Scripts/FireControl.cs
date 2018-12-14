26f02c387fc157c02aaee866ed4f5dd141a65bf1
Leo Gajardo <leonardo@gajardo.com.br>
2018-12-14 07:19:06 -0200

    saving point

--- Assets/Scripts/FireControl.cs
+++ Assets/Scripts/FireControl.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FireControl : MonoBehaviour
@@ -8,15 +9,30 @@ public class FireControl : MonoBehaviour
     public Vector3 positionColl;
     public List<ParticleCollisionEvent> collisionEvents;
     public int colisionNumber;
+    public bool burningTree;
+    public ParticleSystem ParticleSystemEffectBase;
+    public ParticleSystem ThisFireEffect;
+    public Vector3 InitialFire;
+    public List<TreeNode> Vertices;
+    private float waitTime = 0.5f;
+    private float timer = 0.0f;
 
     void Start()
     {
         thisTree = GetComponent<MeshFilter>();
         collisionEvents = new List<ParticleCollisionEvent>();
+        Vertices = thisTree.mesh.vertices.Select((v, i) => new TreeNode() { vertice = transform.TransformPoint(v), isBurning = false, index = i }).ToList();
+        var colors = new Color[thisTree.mesh.vertices.Count()];
+        for (int i = 0; i < thisTree.mesh.vertices.Count() ; i++)
+            colors[i] = new Color(1, 1, 1, 1f);
+
+        thisTree.mesh.colors = colors;
     }
 
     void OnParticleCollision(GameObject other)
     {
+        if (burningTree)
+            return;
         ParticleSystem ps = other.GetComponent<ParticleSystem>();
         int numCollisionEvents = ps.GetCollisionEvents(thisTree.gameObject, collisionEvents);
 
@@ -27,7 +43,6 @@ public class FireControl : MonoBehaviour
         bool shouldDestroy = false;
         int acceptColision = 0;
 
-        //Debug.Log("Collisions " + numCollisionEvents);
         if (colisionNumber < 10)
         {
             acceptColision = 3;
@@ -46,13 +61,14 @@ public class FireControl : MonoBehaviour
         {
             colisionNumber++;
 
-            Debug.Log("Collisions " + colisionNumber);
-
-            if (ps && collisionEvents[i].colliderComponent.CompareTag("Tree") && b > 85 && (colisionNumber%acceptColision) < 15)
+            if (ps && collisionEvents[i].colliderComponent.CompareTag("Tree") && b > 85 && (colisionNumber % acceptColision) < 15)
             {
                 Vector3 pos = collisionEvents[i].intersection;
-                Burn(pos, ps.transform.parent.gameObject);
-                //shouldDestroy = true;
+                InitialFire = pos;
+                var firstFire = FindNearstVector3(pos);
+                firstFire.isBurning = true;                
+                Burn(thisTree.transform.position, ParticleSystemEffectBase.gameObject);
+                burningTree = true;
             }
             i++;
             numCollisionEvents = 0;
@@ -74,6 +90,85 @@ public class FireControl : MonoBehaviour
         //var newFire = NewFireParticle.GetComponent<ParticleSystem>();
         //var a = newFire.main;
         //a.maxParticles = 1;
-        NewFireParticle.GetComponent<ParticleSystem>().Play();
+        var ps = NewFireParticle.GetComponent<ParticleSystem>();
+        ps.shape.rotation.Set(-90, 0, 0);
+        var emission = ps.emission;
+        emission.enabled = true;
+        ps.Play();
+        this.ThisFireEffect = ps;
     }
+
+    private void Update()
+    {
+        if (!burningTree || Vertices.Count(v => !v.isBurning) == 0)
+            return;
+
+        timer += Time.deltaTime;    
+
+        if (timer > waitTime)
+        {
+            var ver = Vertices.Where(v => v.isBurning).ToArray();
+            for (int i = 0; i < 15; i++)
+            {
+                if (ver.Count() - 1 < i)
+                    return;
+                var v = ver[i];
+                var pos = FindNearstVector3(v.vertice);
+                //pos.isBurning = true;
+                //Debug.Log(pos.vertice.ToString());
+                //Burn(pos.vertice, ParticleSystemEffect.gameObject);
+                var tempColors = thisTree.mesh.colors;
+
+                tempColors[pos.index] = new Color(0.25f, 0.25f, 0.25f);
+                thisTree.mesh.colors = tempColors;
+
+                pos.isBurning = true;
+
+                var shape = this.ThisFireEffect.shape;
+                var main = this.ThisFireEffect.main;
+
+                if (shape.radius > 0.4 || shape.length > 5)
+                {
+                    shape.radius = 0.8f;
+                    shape.length = 5f;
+                    main.maxParticles = 100;
+                }
+                else
+                {
+                    shape.radius += 0.05f;
+                    shape.length += 0.2f;
+                    main.maxParticles += 5;
+                }
+            }
+
+            timer = timer - waitTime;
+        }
+    }
+
+    private TreeNode FindNearstVector3(Vector3 position)
+    {
+        var near = new TreeNode();
+        float mindistance = 0;
+
+        foreach (var v in Vertices.Where(v => !v.isBurning))
+        {
+            var distance = Vector3.Distance(v.vertice, position);
+            if((mindistance > distance) || mindistance == 0)
+            {
+                distance = mindistance;
+                near = v;
+            }
+                
+        }
+
+        return near;
+    }
+
+}
+
+public class TreeNode
+{
+    public Vector3 vertice;
+    public bool isBurning;
+    public int index;
 }
---
+++ Assets/Shaders/TreeColoring.shader
@@ -0,0 +1,33 @@
+﻿Shader "Custom/NewShader" {
+    Properties {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _RedColor ("color for red part", Color) = (1,1,0,1)
+        _GreenColor ("color for green part", Color) = (1,1,1,1)
+        _BlueColor ("color for blue part", Color) = (0,0,0,1)
+
+    }
+    SubShader {
+        Tags { "Queue"="Transparent" }
+ 
+        CGPROGRAM
+        #pragma surface surf Lambert alpha
+ 
+        sampler2D _MainTex;
+        half4 _RedColor;
+        half4 _GreenColor;
+        half4 _BlueColor;
+
+        struct Input {
+            float2 uv_MainTex;
+			
+        };
+ 
+        void surf (Input IN, inout SurfaceOutput o) {
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex);
+            o.Emission = fixed4(_RedColor * c.r + _GreenColor * c.g + _BlueColor * c.b);  
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
